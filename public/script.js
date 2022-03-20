c175b5dff1b74d49e5a7455b595eed31ee75d6c9
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-03-20 01:53:31 -0300

    add curriculo e carrosel.

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
+const navItem = document.querySelectorAll('.nav-item')
 
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list") ? menuMobile.classList.replace("bi-list", "bi-x") : menuMobile.classList.replace("bi-x", "bi-list")
 
     body.classList.toggle("menu-nav-active")
+}) 
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
 })
